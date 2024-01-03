12d96156e436ecca01346d7e6b4fb3bdcffd30d2
Fahri Desrian <117554206+Yuuuuurei@users.noreply.github.com>
2024-01-03 11:53:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://pythonuser:[user]@localhost/[db name]'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://[user]:[passwd]@localhost/[db name]'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
